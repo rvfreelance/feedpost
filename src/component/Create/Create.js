f793bc6c2d59d396793ef14c590fc74da2dfdf72
rvfreelance <rvfreelance@outlook.com>
2020-12-20 23:24:49 +0530

    updated image upload

--- src/component/Create/Create.js
+++ src/component/Create/Create.js
@@ -1,39 +1,125 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './Create.scss';
 
-const Create =() =>{
+// function preview() {
+// }
+
+const ImageUpload =(props) =>{
+    const { image, setImage } = props;
+
+    const [ imagePreviewUrl, setImagePreviewUrl ] = useState('');
+    let fileInput = React.createRef();
+
+    const handleImageChange =(e) =>{
+    
+        e.preventDefault();
+    
+        const read = new FileReader();
+    
+        const imageFile = e.target.files[0];
+    
+        read.onloadend =() =>{
+            const image = read.result;
+            setImage(image);
+            setImagePreviewUrl(image);
+        };
+    
+        read.readAsDataURL(imageFile);
+    }
+
+    const handleClick =() =>{
+        fileInput.current.click();
+    }
+
+    const handleRemove =() =>{
+        setImage('');
+        setImagePreviewUrl('');
+        fileInput.current.value = null;
+    };
+    
     return(
-        <div className='create-feed'>
-            <div className='create'>
-                {/* <div>
-                </div> */}
-                <form className='grid-row-full' onSubmit={(e)=>{e.preventDefault();}}>
-                    <span id='feed-title'>feed a new Post</span>
-                    <input 
-                        type='text'
-                        placeholder='Add a Post Title'
-                    />
-                    <input 
-                        type='text'
-                        placeholder='Breifly describe the post'
-                    />
-                    <input 
-                        type='url'
-                        placeholder='Paste the link to the original post'
-                    />
-                    <label className="custom-file-upload">
-                        <input type="file"/>
-                        <span className='custom-title'>Add image</span>
-                        <span style={{fontSize:'0.6rem', textDecoration:'none'}}>(.jpg/ .jpeg/ .png only)</span>
-                    </label>
-                    <div>
-                        <button className='post-button feed-post pointer click-animation' type='submit'>feedPost</button>
-                    </div>
-                </form>
-            </div>
+        <div>
+            {/* <input 
+                type='file'
+                onChange={handleImageChange}
+                ref={fileInput}
+            /> */}
+            <label className="custom-file-upload">
+                <input type="file" onChange={handleImageChange} ref={fileInput}/>
+                <img
+                    className={imagePreviewUrl? '':'hidden'} 
+                    src={imagePreviewUrl} width="150px"
+                />
+
+                <div style={{display:'flex', justifyContent:'space-between', width:'150px'}}>
+                    <span className='custom-title' onClick={()=>handleClick()}>{imagePreviewUrl? 'Change':'Add image'}</span>
+                    <span className={imagePreviewUrl? 'custom-title':'hidden'} onClick={()=>handleRemove()}>Remove</span>
+                </div>
+                <span style={{fontSize:'0.6rem', textDecoration:'none'}}>(.jpg/ .jpeg/ .png only)</span>
+            </label>
         </div>
     )
+}
+
+const Create=() =>{
+    // constructor(props){
+    //     super(props);
+    //     this.state={
+    //         file: null,
+    //     }
+    // }
+    const [image, setImage] = useState('');
+    // handleChange =(event) =>{
+    //     this.setState({ file: URL.createObjectURL(event.target.files[0]) });
+    // }
+
+    // render(){
+        return(
+            <div className='create-feed'>
+                <div className='create'>
+                    {/* <div>
+                    </div> */}
+                    <form id='create-post-form' onSubmit={(e)=>{e.preventDefault();}}>
+                        <span id='feed-title'>feed in</span>
+                        <div className='onebytwo pbtm40'>
+                            <div className='grid-row-full'>
+                                <input 
+                                    type='text'
+                                    placeholder='Post title'
+                                />
+                                <input 
+                                    type='text'
+                                    placeholder='Description'
+                                />
+                                <input 
+                                    type='url'
+                                    placeholder='Link to published post'
+                                />
+                            </div>
+                            <ImageUpload setImage={setImage} image={image}/>
+                            {/* <label className="custom-file-upload">
+                                <input type="file" onChange={(e)=> this.handleChange(e)}/>
+                                <img
+                                    className={this.state.file? '':'hidden'} 
+                                    src={this.state.file} width="150px"
+                                />
+
+                                <div style={{display:'flex', justifyContent:'space-between', width:'150px'}}>
+                                    <span className='custom-title'>{this.state.file? 'Change':'Add image'}</span>
+                                    <span className={this.state.file? 'custom-title':'hidden'} onClick={(e)=>e.preventDefault()}>Remove</span>
+                                </div>
+                                <span style={{fontSize:'0.6rem', textDecoration:'none'}}>(.jpg/ .jpeg/ .png only)</span>
+                            </label> */}
+                        </div>
+                        <div>
+                            <button className='post-button feed-post pointer click-animation' type='submit'>feedPost</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        )
+    // }
 }
 
 export default Create;
