8d5828fca161bf75b21148d7de8b3cd2bfc589ab
rvfreelance <rvfreelance@outlook.com>
2021-01-11 21:27:25 +0530

    removed the login issue statement

--- src/component/Login/Login.js
+++ src/component/Login/Login.js
@@ -89,8 +89,6 @@ const LoginEmailForm =(props) =>{
                         Select Login Method
                         <br/>
                         <br/>
-                        <span style={{fontSize:'0.8rem', color:'orange'}}>Currently facing some issues signing in with Google</span>
-                        <br/>
                         <br/>
                         <button className='x-post-button' 
                             style={{color:'#4b4b4b', fontSize:'2.2rem'}} 
